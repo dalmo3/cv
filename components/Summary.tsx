4c5325db80a24ea6c056485cee11b8b31d535352
Dalmo Mendonça <dalmo3@gmail.com>
2023-05-28 11:44:34 +1200

    udpate summary

--- components/Summary.tsx
+++ components/Summary.tsx
@@ -5,10 +5,10 @@ export const Summary: VFC = () => (
   <div>
     <H2>Summary</H2>
     <p>
-      Senior Frontend Developer exploring the latest web technologies. Full
-      stack experience. Bachelor of Computer Engineering with strong background
-      in Computer Science and extensive experience in developing the most
-      diverse software projects.
+      Senior Frontend Developer consistently delivering value. Full stack
+      experience. Bachelor of Computer Engineering with strong background in
+      Computer Science and extensive experience in developing diverse software
+      projects.
     </p>
   </div>
 );
